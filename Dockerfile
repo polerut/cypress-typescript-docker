02a112c06294bdafec98b86f56703bab96efc69a
polerut <giraf.2011@list.ru>
2025-02-09 21:23:31 -0700

    Обновления в проекте

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,10 @@
-# Используем официальный образ Cypress с Chrome и Firefox
 FROM cypress/included:12.17.2
 
-# Устанавливаем рабочую директорию
 WORKDIR /e2e
 
-# Копируем файлы проекта
 COPY package.json package-lock.json ./
 RUN npm install
 
-# Копируем весь проект
 COPY . .
 
-# Запуск тестов при старте контейнера
 CMD ["npx", "cypress", "run"]
--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,13 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on("after:screenshot", (details) => {
+        console.log(`📸 Скриншот сохранен: ${details.path}`);
+      });
     },
+    screenshotOnRunFailure: false,
+    screenshotsFolder: "cypress/screenshots",
+    video: false,
+    videosFolder: "cypress/videos",
   },
 });
